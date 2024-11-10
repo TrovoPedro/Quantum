92dc12d2084528686e83ff41ba29ed4b0238c444
Pedro Trovo <pedro.souza400@etec.sp.gov.br>
2024-11-09 21:16:53 -0300

    Finalizando front-end da dashboardTrovo

---
+++ public/js/dashTrovo.js
@@ -0,0 +1,16 @@
+function voltarHome(){
+    window.location.href = "dashboardComponenteGeral.html";
+}
+
+function enviarFormulario(){
+
+}
+
+function verPerfil(){
+    window.location.href = "../Tela_Perfil.html";
+}
+
+function sair(){
+    sessionStorage.clear();
+    window.location = "../login.html";
+}
