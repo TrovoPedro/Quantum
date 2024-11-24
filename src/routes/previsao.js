bb829856d3554a1c28f0fa39badc8095711d79e1
RebecaCagni <rebeca.cagni@sptech.school>
2024-11-24 00:54:29 -0300

    dashboard individual finalizada

--- src/routes/previsao.js
+++ src/routes/previsao.js
@@ -7,7 +7,7 @@ router.get("/buscarPorId", function (req, res) {
     previsaoController.buscarPorId(req, res);
 });
 
-router.get("/tendenciaUsoRam", function (req, res) {
+router.get("/buscarTendenciaUsoRam/:componenteId/:servidorId", function (req, res) {
     previsaoController.buscarTendenciaUsoRam(req, res);
 });
 
@@ -15,12 +15,25 @@ router.get("/buscarHeatmap", function (req, res) {
     previsaoController.buscarHeatmap(req, res);
 });
 
-router.get("/buscarSelect", function (req, res) {
-    previsaoController.buscarSelect(req, res);
-});
-
 router.get("/buscarComponente", function (req, res) {
     previsaoController.buscarComponente(req, res);
+});
+
+router.get("/buscarmediaRam", function (req, res) {
+    previsaoController.buscarmediaRam(req, res);
+});
+
+router.get("/buscarmediaCpu", function (req, res) {
+    previsaoController.buscarmediaCpu(req,res);
+});
+
+
+router.get("/buscarpicoCpu", function (req, res) {
+    previsaoController.buscarpicoCpu(req,res);
+});
+
+router.get("/buscarpicoRam", function (req, res) {
+    previsaoController.buscarpicoRam(req,res);
 });
 
 module.exports = router;
