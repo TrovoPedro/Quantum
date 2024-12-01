ca463e680206c20f9e99c895c52b40695ad80cd9
NataliaAlmeidadS <nataliareg76@gmail.com>
2024-11-30 22:59:35 -0300

    arrumando css da tela do tecnico

--- CapturaKotlin/src/main/kotlin/repositorio/DadosRepositorio.kt
+++ CapturaKotlin/src/main/kotlin/repositorio/DadosRepositorio.kt
@@ -11,9 +11,9 @@ class DadosRepositorio {
     fun configurar(){
         val dataSource = BasicDataSource();
         dataSource.driverClassName = "com.mysql.cj.jdbc.Driver";
-        dataSource.url = "jdbc:mysql://localhost/QuantumDB?useSSL=false&serverTimezone=America/Sao_Paulo"
+        dataSource.url = "jdbc:mysql://54.173.61.202/QuantumDB?useSSL=false&serverTimezone=America/Sao_Paulo"
         dataSource.username = "root"
-        dataSource.password = "Semsenha13"
+        dataSource.password = "@Davidson01"
 
         jdbcTemplate = JdbcTemplate(dataSource);
     }
